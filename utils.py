0a8e82b43abeb3071389eec8682275fbe80ed6d0
pfernandom <pmarquez3@gatech.edu>
2022-11-14 21:42:46 -0600

    Update minst

--- utils.py
+++ utils.py
@@ -48,6 +48,9 @@ class NoiseUtil:
     @staticmethod
     def random_pixel_noise(img2, filter_size, patch_size, filter_fn, shape):
         
+        if filter_size == 0:
+            return img2
+        
         if (len(tf.shape(img2)) < 4):
             img2 = tf.convert_to_tensor(img2[:,:,:])
             img2 = tf.expand_dims(img2, axis=0)
@@ -55,74 +58,41 @@ class NoiseUtil:
             img2 = tf.convert_to_tensor(img2[:,:,:,:])
   
         img2_filtered = filter_fn(img2)
-        print(f"shape={shape}")
         shp = shape
         n = shp[0]
         h = shp[1]
         w = shp[2]
         c = shp[3]
 
-        print(h)
-
-        # mask = build_negative_filter(n, h, w, c, filter=patch_size, masks=filters)
-        # x = tf.random.uniform(
-        #     tf.shape(img2),
-        #     minval=0,
-        #     maxval=2,
-        #     dtype=tf.int32,
-        # )
         mask = tf.reshape(tf.range(h*w*c), (h,w,c))
 
+        random.seed(time.time())
         i = random.randint(0,h-patch_size)
         j = random.randint(0,w-patch_size)
-
-        a = tf.math.logical_and(mask / h <= j+patch_size, mask % h <= i+patch_size)
-        b = tf.math.logical_and(mask / w > j, mask % w > i)
+        
+        a = tf.math.logical_and(mask / h <= j+patch_size, mask % w <= i+patch_size)
+        b = tf.math.logical_and(mask / h > j, mask % w > i)
         d = tf.math.logical_and(a,b)
 
-        for i in range(filter_size):
+        for i in range(filter_size-1):
             i = random.randint(0,h-patch_size)
             j = random.randint(0,w-patch_size)
 
-            a = tf.math.logical_and(mask / h <= j+patch_size, mask % h <= i+patch_size)
-            b = tf.math.logical_and(mask / w > j, mask % w > i)
+            a = tf.math.logical_and(mask / h <= j+patch_size, mask % w <= i+patch_size)
+            b = tf.math.logical_and(mask / h > j, mask % w > i)
             c = tf.math.logical_and(a,b)
             d = tf.math.logical_or(d,c)
 
-
-        # im = tf.reshape(tf.constant(range(2*12*12*1)), (2,12,12,1))
         img3 = tf.where(d, tf.zeros_like(img2), img2)
-        # img3 = tf.where(x == 0, img2, tf.zeros_like(img2))
-
-        # img3 = tf.math.multiply(img2, mask)
+        trans_img3 = tf.where(d, tf.zeros_like(img2_filtered), img2_filtered)
         img4 = img2_filtered * 2
-        img2 = img3 + img4 - img2_filtered
-
-
-        # starts = random.sample(range(10000000), filters)
-        
-        # for i in range(filters):
-        #     assert patch_size > 0
-        #     assert patch_size % 2 == 0
-        #     assert patch_size < img2.shape[1]-4
-        #     assert patch_size < img2.shape[2]-4
-
-        #     random.seed(time.time())
-
-        #     s1 = random.randint(0,img2.shape[1]-patch_size-4)
-        #     s2 = random.randint(0,img2.shape[2]-patch_size-4)
-            
-        #     img3 = tfa.image.cutout(img2, mask_size=[patch_size,patch_size], offset= [s1,s2])
-        #     img4 = tfa.image.cutout(img2_filtered, mask_size=[patch_size,patch_size], offset= [s1,s2])
-
-        #     img2 = img3 + img2_filtered - img4
+        img2 = img3 + img2_filtered - trans_img3
         
         return img2
 
     @staticmethod
     def pixel_noise(img, filters, filter_size):
-        return NoiseUtil.random_pixel_noise(img, filters, filter_size, filter_fn=NoiseUtil.filter_pixel(downsize_image_ratios = [1/4]), shape=img.shape)
-
+        return NoiseUtil.random_pixel_noise(img, filters, filter_size, filter_fn=NoiseUtil.filter_pixel(downsize_image_ratios = [1/2]), shape=img.shape)
 
 class ImgUtils():
     @staticmethod
@@ -160,6 +130,15 @@ class DataLoader():
         output_signature=tf.TensorSpec(shape=shape, dtype=tf.float32)
         return tf.data.Dataset.from_generator(fn, output_signature=output_signature).map(lambda x: (x,x))
 
+# class DataManagerBuilder:
+#     def with_train_data(data):
+#         self.train_ds = data
+        
+#     def with_test_data(data):
+#         self.test_ds = data
+        
+#     def with_
+    
 class DataManager():
     def __init__(self, train_ds, test_ds = None):
         self.train_ds = train_ds
@@ -172,9 +151,9 @@ class DataManager():
         self.transform = fn
 
     @staticmethod
-    def create_label_with_input_transform(train_generator, test_generator, input_shape, transform_fn=lambda x:x, test_samples=25):
+    def create_label_with_input_transform(train_generator, test_generator, input_shape, transform_fn=lambda x:x, test_samples=50):
         def normalize_both(x,y):
-            return ImgUtils.normalize(x), ImgUtils.normalize(y)
+            return ImgUtils.normalize(x, as_probs=True), ImgUtils.normalize(y, as_probs=True)
         
         train_ds = DataLoader.two_from_generator(train_generator, input_shape).map(normalize_both).skip(test_samples)
         test_ds = None
@@ -194,7 +173,6 @@ class DataManager():
         return self.train_ds.take(batch_size).batch(batch_size).map(self.transform)
 
     def print_validation(self, model=lambda x:x, batch_size=5, save=False, path="./"):
-        random.seed(10)
         rows = batch_size
         cols = 2
 
@@ -231,7 +209,84 @@ class DataManager():
 
 
 # def add_noise(x,y):
-#     n = NoiseUtil.pixel_noise(x, 30, 2)
+#     n = NoiseUtil.pixel_noise(x, 25, 5)
+
+# #     n = x + 0.4 * tf.random.normal(
+# #         x.shape[1:],
+# #         mean=0.0,
+# #         stddev=1.0,
+# #         dtype=tf.dtypes.float32,
+# #     )
+
 #     return n,y
 # dm = DataManager.create_label_with_input_transform(train_get, test_get, (28,28,1), add_noise)
 # dm.print_validation() 
+
+
+class SSIM(tf.keras.metrics.Metric):
+
+  def __init__(self, name='ssim', **kwargs):
+    super(SSIM, self).__init__(name=name, **kwargs)
+    self.ssim = self.add_weight(name='ssim', initializer='zeros')
+    self.ep = 0.0000001
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    y_true = ImgUtils.denormalize(y_true, cast=tf.uint8)
+    y_pred = ImgUtils.denormalize(y_pred, cast=tf.uint8)
+    same = tf.math.reduce_sum(tf.image.ssim(y_true, y_true, 255.0, filter_size=3)) + self.ep
+    values = (self.ep + tf.math.reduce_sum(tf.image.ssim(y_true, y_pred, 255.0, filter_size=3))) / same
+    
+    values = tf.cast(values, self.dtype)
+    if sample_weight is not None:
+        sample_weight = tf.cast(sample_weight, self.dtype)
+        sample_weight = tf.broadcast_to(sample_weight, values.shape)
+        values = tf.multiply(values, sample_weight)
+    self.ssim.assign(tf.reduce_sum(values))
+
+  def result(self):
+    return self.ssim
+
+class SSIM_Multiscale(tf.keras.metrics.Metric):
+
+  def __init__(self, name='ssim_ms', **kwargs):
+    super(SSIM_Multiscale, self).__init__(name=name, **kwargs)
+    self.ssim_ms = self.add_weight(name='ssim_ms', initializer='zeros')
+    self.self_ssim_ms = self.add_weight(name='self_ssim_ms', initializer='zeros')
+    self.ep = 0.0000001
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    y_true = ImgUtils.denormalize(y_true, cast=tf.uint8)
+    y_pred = ImgUtils.denormalize(y_pred, cast=tf.uint8)
+    same = tf.math.reduce_sum(tf.image.ssim_multiscale(y_true, y_true, 255.0, filter_size=3)) + self.ep
+    values = (self.ep + tf.math.reduce_sum(tf.image.ssim_multiscale(y_true, y_pred, 255.0, filter_size=4))) / same
+
+    values = tf.cast(values, self.dtype)
+    if sample_weight is not None:
+        sample_weight = tf.cast(sample_weight, self.dtype)
+        sample_weight = tf.broadcast_to(sample_weight, values.shape)
+        values = tf.multiply(values, sample_weight)
+    self.ssim_ms.assign(values)
+    self.self_ssim_ms.assign(same)
+
+  def result(self):
+    return self.ssim_ms
+
+class TOP_SSIM_Multiscale(tf.keras.metrics.Metric):
+
+  def __init__(self, name='self_ssim_ms', **kwargs):
+    super(TOP_SSIM_Multiscale, self).__init__(name=name, **kwargs)
+    self.self_ssim_ms = self.add_weight(name='self_ssim_ms', initializer='zeros')
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    y_true = ImgUtils.denormalize(y_true, cast=tf.uint8)
+    values = tf.math.reduce_sum(tf.image.ssim_multiscale(y_true, y_true, 255.0, filter_size=3))
+
+    values = tf.cast(values, self.dtype)
+    if sample_weight is not None:
+        sample_weight = tf.cast(sample_weight, self.dtype)
+        sample_weight = tf.broadcast_to(sample_weight, values.shape)
+        values = tf.multiply(values, sample_weight)
+    self.self_ssim_ms.assign(values)
+
+  def result(self):
+    return self.self_ssim_ms
